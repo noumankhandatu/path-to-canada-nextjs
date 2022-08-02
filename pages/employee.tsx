52368e9b09c32030bab4dcfb5786dde05795c23d
noumankhandatu <noumankhandatu>
2022-08-02 14:42:10 +0500

    employee page dynamic

--- pages/employee.tsx
+++ pages/employee.tsx
@@ -3,48 +3,169 @@ import HomePageFirstSection from "../scr/components/Organism/homePageFirstSectio
 import BlackBannerLeafSection from "../scr/components/Organism/blackBannerLeafSection";
 import Wrapper from "../scr/components/Organism/Wrapper";
 import EmployeeCard from "../scr/components/Molecules/employeeCard";
-import EmployeeCardArray from "../scr/components/array/employeeCardArray";
 import Input from "../scr/components/Atoms/input";
 import Button from "../scr/components/Atoms/button";
 import Font from "../scr/components/Atoms/Font";
-const Employee = () => {
+import Prismic from "prismic-javascript";
+import { Client } from "../prismic-configuration";
+const Employee = ({ employee }: any) => {
+  const mapper = employee?.results?.map((items) => {
+    return items?.data;
+  });
+  // home banner started
+  const backgroundImage = mapper.map((data) => {
+    return data.slices[1]?.items[0]?.backgroundImage.url;
+  });
+  const title = mapper.map((data) => {
+    return data.slices[1]?.items[0]?.title;
+  });
+  const description = mapper.map((data) => {
+    return data.slices[1]?.items[0]?.description;
+  });
+  // home banner ended
+  // employe started
+  const imageOne = mapper?.map((data) => {
+    return data?.imageOne?.url;
+  });
+  const titleOne = mapper?.map((data) => {
+    return data?.title;
+  });
+  const descriptionOne = mapper?.map((data) => {
+    return data?.description;
+  });
+
+  const linkOne = mapper?.map((data) => {
+    return data?.linkOne;
+  });
+  const linkTwo = mapper?.map((data) => {
+    return data?.linkTwo;
+  });
+  const linkThree = mapper?.map((data) => {
+    return data?.linkThree;
+  });
+  const headingOne = mapper?.map((data) => {
+    return data?.headingOne;
+  });
+  const headingTwo = mapper?.map((data) => {
+    return data?.headingTwo;
+  });
+  const headingThree = mapper?.map((data) => {
+    return data?.headingThree;
+  });
+  const paragraphOne = mapper?.map((data) => {
+    return data?.paragraphOne;
+  });
+  const paragraphTwo = mapper?.map((data) => {
+    return data?.paragraphTwo;
+  });
+  const paragraphThree = mapper?.map((data) => {
+    return data?.paragraphThree;
+  });
+
+  const EmployeeCardArray = [
+    {
+      heading: headingOne ? headingOne : `pending`,
+      paragraph: paragraphOne ? paragraphOne : `pending`,
+      link: linkOne ? linkOne : `pending`,
+    },
+    {
+      heading: headingTwo ? headingTwo : `pending`,
+      paragraph: paragraphTwo ? paragraphTwo : `pending`,
+      link: linkTwo ? linkTwo : `pending`,
+    },
+    {
+      heading: headingThree ? headingThree : `pending`,
+      paragraph: paragraphThree ? paragraphThree : `pending`,
+      link: linkThree ? linkThree : `pending`,
+    },
+  ];
+  const textOne = mapper.map((data) => {
+    return data.slices[0]?.items[0]?.textOne;
+  });
+  const textTwo = mapper.map((data) => {
+    return data.slices[0]?.items[0]?.textTwo;
+  });
+  const textThree = mapper.map((data) => {
+    return data.slices[0]?.items[0]?.textThree;
+  });
+  const textFour = mapper.map((data) => {
+    return data.slices[0]?.items[0]?.textFour;
+  });
+  const textFive = mapper.map((data) => {
+    return data.slices[0]?.items[0]?.textFive;
+  });
+  const textSix = mapper.map((data) => {
+    return data.slices[0]?.items[0]?.textSix;
+  });
+  const textSeven = mapper.map((data) => {
+    return data.slices[0]?.items[0]?.textSeven;
+  });
+  const textEight = mapper.map((data) => {
+    return data.slices[0]?.items[0]?.textEight;
+  });
+
+  const profileTextOne = mapper.map((data) => {
+    return data.slices[0]?.items[0]?.profileTextOne;
+  });
+  const profileTextTwo = mapper.map((data) => {
+    return data.slices[0]?.items[0]?.profileTextTwo;
+  });
+  const profileTextThree = mapper.map((data) => {
+    return data.slices[0]?.items[0]?.profileTextThree;
+  });
+  const profileTextFour = mapper.map((data) => {
+    return data.slices[0]?.items[0]?.profileTextFour;
+  });
+  const profileTextFive = mapper.map((data) => {
+    return data.slices[0]?.items[0]?.profileTextFive;
+  });
+  const profileTextSix = mapper.map((data) => {
+    return data.slices[0]?.items[0]?.profileTextSix;
+  });
+  const profileTextSeven = mapper.map((data) => {
+    return data.slices[0]?.items[0]?.profileTextSeven;
+  });
+  const profileTextEight = mapper.map((data) => {
+    return data.slices[0]?.items[0]?.profileTextEight;
+  });
+  const profileTextNine = mapper.map((data) => {
+    return data.slices[0]?.items[0]?.profileTextNine;
+  });
+  const backgroundImageOne = mapper.map((data) => {
+    return data.slices[0]?.items[0]?.backgroundImage.url;
+  });
+
+  const bannerTextOne = mapper.map((data) => {
+    return data.bannerTextOne;
+  });
+  const bannerTextTwo = mapper.map((data) => {
+    return data.bannerTextThree;
+  });
+  const bannerTextThree = mapper.map((data) => {
+    return data.bannerTextTwo;
+  });
+  const bannerBackgroundImage = mapper.map((data) => {
+    return data.bannerBackgroundImage.url;
+  });
   return (
     <div className="pt-40">
       <HomePageFirstSection
-        heading={`Build your tech career and life in Canada`}
-        paragraph={
-          "Canadian tech companies are hiring and looking for experienced talent."
-        }
-        bgImage={`https://path2canada.ca/wp-content/uploads/2022/05/employees_hero.svg`}
+        heading={title ? title : `pending`}
+        paragraph={description ? description : `pending`}
+        bgImage={backgroundImage ? backgroundImage : `pending`}
         showFirstBtn={true}
       />
       <Wrapper className="bg-employee-Color pt-5 pb-20">
         <div className="grid lg:grid-cols-2 grid-cols-1 gap-10  pt-20 ">
           <div>
-            <img
-              src="https://path2canada.ca/wp-content/uploads/2022/05/employees_manInFrontOfComputer.svg"
-              alt=""
-            />
+            <img src={imageOne ? imageOne : `pending`} alt="" />
           </div>
           <div>
             <div className="text-4xl font-bold  text-cyan-900">
-              Facing immigration or permanent resident challenges in the US?
+              {titleOne ? titleOne : `pending`}
             </div>
             <Font className="pt-5">
-              Canada may be the answer to your immigration dilemma – and your
-              career dreams. The reality of the H-1B immigration situation in
-              the US is that many more candidates will apply than those who will
-              get selected. Unfortunately, after finding your dream job and
-              starting a life in the US, losing out on the lottery means that
-              you will not have a chance to work and live in the US long-term.
-              Fortunately, there’s a job explosion happening in Canada, that you
-              can take advantage of.
-            </Font>
-            <Font className="pt-5">
-              Our team at Path to Canada focuses on matching highly skilled tech
-              professionals with Canadian tech companies who are hiring and will
-              sponsor your work visa. We are expertly focused on the North
-              American tech marketplace. Our goal is to get you hired.
+              {descriptionOne ? descriptionOne : `pending`}
             </Font>
           </div>
         </div>
@@ -57,75 +178,83 @@ const Employee = () => {
       <Wrapper className="bg-form-color">
         <div
           style={{
+            backgroundImage: `url(${
+              backgroundImageOne ? backgroundImageOne : null
+            })`,
             backgroundSize: "40% 59%",
             backgroundPosition: "left 25% bottom -27%",
           }}
-          className="pb-20 pt-20 bg-[url('https://path2canada.ca/wp-content/uploads/2022/05/employees_arrowMan_white.svg')] bg-cover	 bg-no-repeat  "
+          className="pb-20 pt-20 bg-cover	 bg-no-repeat  "
         >
           <div className="grid lg:grid-cols-[0.7fr_1fr]  grid-cols-1 text-white gap-32">
             <div>
               <div className="lg:text-4xl text-3xl font-bold  ">
-                Ready to move to Canada to continue your tech career?
+                {textOne ? textOne : `pending`}
               </div>
               <div className="text-2xl pt-10 font-bold  ">
-                Qualification is quick and easy.
+                {textTwo ? textTwo : `pending`}
               </div>
-              <Font className="text-lg pb-6 pt-2 font-bold  ">
-                Key criteria include:
-              </Font>
               <Font>
                 <li className="list-disc  ">
-                  Willing to move your tech career and life to Canada
+                  {textThree ? textThree : `pending`}
                 </li>
                 <li className="list-disc  ">
-                  A minimum of 3 years’ experience in a tech qualified role
+                  {textFour ? textFour : `pending`}
                 </li>
-                <li className="list-disc  ">Tech Degree is preferred</li>
+                <li className="list-disc  "> {textFive}</li>
               </Font>
               <div className="text-2xl pt-10 font-bold  text-white">
-                SUBMIT YOUR PROFILE TODAY!
+                {textSix ? textSix : `pending`}
               </div>
-              <Font className="pt-4">
-                Your first step is to complete the Candidate Assessment Form. Be
-                sure to answer every question as carefully and accurately as
-                possible.
-              </Font>
-              <Font className=" pt-4">
-                Take 5 minutes to answer a few questions — Let’s get started.
-              </Font>
+              <Font className="pt-4">{textSeven ? textSeven : `pending`}</Font>
+              <Font className=" pt-4">{textEight ? textEight : `pending`}</Font>
             </div>
             {/* form below */}
             <div>
               <div className="text-center p-10 bg-red-600">
                 <div className="lg:text-4xl text-3xl font-bold  text-white">
-                  SUBMIT YOUR PROFILE TODAY!
+                  {profileTextOne ? profileTextOne : `pending`}
                 </div>
                 <div className="pt-5">
-                  Let’s work together to find your next role in Canada
+                  {profileTextTwo ? profileTextTwo : `pending`}
                 </div>
               </div>
               <div className=" p-10 bg-white text-black">
                 <div className="lg:text-5xl text-3xl font-bold  text-aqua-color">
-                  Candidate Assessment
+                  {profileTextThree ? profileTextThree : `pending`}
                 </div>
                 <Font className="pt-10 pb-10">
-                  Let this be the start of a new life for you in Canada. Please
-                  complete your profile as accurately and completely as possible
-                  to allow us to quickly determine your qualification for our
-                  Tech Talent Relocation Program.
+                  {profileTextFour ? profileTextFour : `pending`}
                 </Font>
                 <hr />
                 <div className="pt-5">
-                  <div className="text-base font-bold pb-10">Name*</div>
+                  <div className="text-base font-bold pb-10">
+                    {profileTextFive ? profileTextFive : `pending`}
+                  </div>
                   <div className="grid grid-cols-1 md:grid-cols-2  gap-3 pb-5">
-                    <Input text="First" width={"w-full"} toggle={true} />
-                    <Input text="Last " width={"w-full"} toggle={true} />
+                    <Input
+                      text={profileTextSix ? profileTextSix : `pending`}
+                      width={"w-full"}
+                      toggle={true}
+                    />
+                    <Input
+                      text={profileTextSeven ? profileTextSeven : `pending`}
+                      width={"w-full"}
+                      toggle={true}
+                    />
                   </div>
                   <div className="pt-5">
-                    <Input text="Email" width={"w-full"} toggle={true} />
+                    <Input
+                      text={profileTextEight ? profileTextEight : `pending`}
+                      width={"w-full"}
+                      toggle={true}
+                    />
                   </div>
                   <div className="pt-10 pb-5">
-                    <Button text="NEXT" primary />
+                    <Button
+                      text={profileTextNine ? profileTextNine : `pending`}
+                      primary
+                    />
                   </div>
                 </div>
               </div>
@@ -141,34 +270,20 @@ const Employee = () => {
         <div className="pt-20 relative  grid lg:grid-cols-[0.7fr_1fr_1fr]  grid-cols-1 gap-2">
           <div>
             <div className="lg:text-6xl text-3xl font-bold lg:tracking-wide text-cyan-900	">
-              What happens once you submit your profile?
+              {bannerTextOne ? bannerTextOne : `pending`}
             </div>
           </div>
           <div>
             <div className="break-words tracking-wide text-lg	 font-bold pb-5">
-              Our team will first determine your qualification status. We will
-              then actively partner with you and match you to current or future
-              open roles.
+              {bannerTextThree ? bannerTextThree : `pending`}
             </div>
             <Font className="pt-4 ">
-              You will be invited to a video call so we can get to know you and
-              understand your specific situation and the type of role you are
-              interested in. In this session, we will share our expertise on the
-              Canadian tech sector, the positions that match your skill sets,
-              and then begin the process of getting you connected and
-              interviewed. Our number one goal is you! We want to ensure you get
-              an amazing role with a great company and with a good salary to
-              match!
-            </Font>
-            <Font className="pt-4">
-              We understand that moving to a new country isn’t easy and you’ll
-              have lots of questions. We are here to support you every step of
-              the way.
+              {bannerTextTwo ? bannerTextTwo : `pending`}
             </Font>
           </div>
           <div>
             <img
-              src="https://path2canada.ca/wp-content/uploads/2022/05/employees_happyWorker.jpg"
+              src={bannerBackgroundImage ? bannerBackgroundImage : `pending`}
               alt=""
             />
           </div>
@@ -180,3 +295,13 @@ const Employee = () => {
 };
 
 export default Employee;
+export async function getServerSideProps() {
+  const employee = await Client().query(
+    Prismic.Predicates.at("document.type", "employee")
+  );
+  return {
+    props: {
+      employee,
+    },
+  };
+}
